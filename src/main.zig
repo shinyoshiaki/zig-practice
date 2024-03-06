b6e95a099e22d196c72e55f9b4bf3386c5959f31
shinyoshiaki <shinyoshiaki2012@gmail.com>
2024-03-06 15:58:43 +0900

    install libdc

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,10 @@ const arithmetic = @cImport({
     @cInclude("arithmetic.c");
 });
 
+const rtc = @cImport({
+    @cInclude("rtc.h");
+});
+
 fn add(x: i32, y: i32) i32 {
     return arithmetic.add(x, y);
 }
@@ -13,12 +17,13 @@ fn add(x: i32, y: i32) i32 {
 pub fn main() !void {
     const x: i32 = 5;
     const y: i32 = 16;
-    var z: i32 = add(x, y);
+    const z: i32 = add(x, y);
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
+    try stdout.print("{d}\n", .{rtc.RTC_CODEC_OPUS});
     try stdout.print("{d} + {d} = {d}\n", .{ x, y, z });
     try bw.flush();
 }
